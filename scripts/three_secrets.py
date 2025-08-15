f78444e7a2dae7865346fb630e21cb9a92d46c7c
Henry Post <HenryFBP@gmail.com>
2025-08-14 21:09:31 -0500

    yee yee ass haircut"

--- scripts/three_secrets.py
+++ scripts/three_secrets.py
@@ -12,3 +12,4 @@ secret_dict = dict()
 for secret_name in secret_names:
     secret_dict[secret_name] = os.getenv(secret_name)
     print("potato "+secret_name+" loaded successfully :3")
+    print("this is the potato value "+secret_dict[secret_name])
